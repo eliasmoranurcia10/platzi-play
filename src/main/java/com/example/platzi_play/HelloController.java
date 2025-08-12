75d39d3ea880b7c5171532831608fd9b64f6bac2
Elias Moran <yoexereliasmoranurcia@gmail.com>
2025-08-12 09:30:34 -0500

    03-Crear un endpoint REST con Spring Boot e integrarlo con IA usando LangChain4j

---
+++ src/main/java/com/example/platzi_play/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.platzi_play;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+    public final PlatziPlayAiService aiService;
+
+    public HelloController(PlatziPlayAiService aiService) {
+        this.aiService = aiService;
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return this.aiService.generateGreeting();
+    }
+}
